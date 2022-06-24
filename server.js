2477454e4a45125e52f9b0e62dab04651cc56d11
amimbs <andymimbs91@gmail.com>
2022-06-23 20:33:50 -0400

    cors errors

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ require ('dotenv').config();
 
 app.use(express.json());
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin", "https://weathaserverreact.herokuapp.com"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
