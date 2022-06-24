5ad54fc6cbab38058934df279ec90035d557aeb4
amimbs <andymimbs91@gmail.com>
2022-06-23 21:11:38 -0400

    cors errors

--- server.js
+++ server.js
@@ -10,12 +10,7 @@ app.use(
     cors()
 );
 
-app.all('*', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-});
+app.all('*',cors())
 
 app.options('*', cors()) 
 
