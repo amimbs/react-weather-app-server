9f189efd9e71bab440438c37c7db6fe980b122a7
amimbs <andymimbs91@gmail.com>
2022-06-23 20:43:12 -0400

    cors errors

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const app = express();
 const cors = require('cors');
+const app = express();
+
 // We do this because the front end runs on 3000
 const PORT = process.env.PORT || 3001;
 // heroku gives a port that the app runs on so we set it equal to the environments process to ensure it deploys correctly
@@ -11,13 +12,10 @@ const models = require('./models');
 const user = require('./models/user')
 require ('dotenv').config();
 
-
 app.use(express.json());
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://weathaserverreact.herokuapp.com"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+app.use(
+    cors()
+);
 
 // routers
 const usersRouter = require('./routes/Users.js');
